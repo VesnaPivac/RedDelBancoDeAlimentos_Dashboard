b4f4f9756a4dba7d6c09f1892d822f5885dd6a15
VesnaPivac <33427475+VesnaPivac@users.noreply.github.com>
2023-12-06 02:54:42 -0600

    Update Red_del_Banco_de_Alimentos.py
    Integrantes

--- Red_del_Banco_de_Alimentos.py
+++ Red_del_Banco_de_Alimentos.py
@@ -4,6 +4,7 @@ import numpy as np
 from PIL import Image
 
 st.title('Red del Banco de Alimentos')
+st.markdown("<span style='font-size: 12px;'>Equipo: Vesna Camile Pivac Alcaraz, Jose Carlos Barreras Maldonado y Luis Ernesto Ortiz Villalon</span>", unsafe_allow_html=True)
 
 body = 'Este proyecto es parte del curso "Ingeniería de Características" de la Maestría en Ciencia de Datos de la Universidad de Sonora. Su motivación principal es contribuir a la Red de Bancos de Alimentos de México (red BAMX) mediante el análisis de datos públicos relacionados con la producción y el mercado de alimentos a nivel nacional. El objetivo final es realizar el "Storytelling" que permita a la red BAMX identificar ventanas de oportunidad para optimizar sus procedimientos en la colecta y redistribución de alimentos.'
 
